ee8c54f6d7f4c5a34b381d3fc66aa0a328fb314a
Alakh Chaturvedi <135260269+Alakh11@users.noreply.github.com>
2023-09-28 17:27:29 +0530

    Add files via upload

--- login.jsx
+++ login.jsx
@@ -4,7 +4,11 @@ export const Login = (props) => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
 
-   const handleSubmit = (e) => { e.preventDefault(); console.log(e); window.alert("You have sucsessfully logged in with " + email); };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
+
     return (
         <div className="auth-form-container">
             <h2>Login</h2>
