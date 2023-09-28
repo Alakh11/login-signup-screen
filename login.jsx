205155298f11d6d43ca1f669c35c36d8276abd39
Alakh Chaturvedi <135260269+Alakh11@users.noreply.github.com>
2023-09-28 16:27:06 +0530

    login.jsx

--- login.jsx
+++ login.jsx
@@ -4,11 +4,7 @@ export const Login = (props) => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(email);
-    }
-
+   const handleSubmit = (e) => { e.preventDefault(); console.log(e); window.alert("You have sucsessfully logged in with " + email); };
     return (
         <div className="auth-form-container">
             <h2>Login</h2>
